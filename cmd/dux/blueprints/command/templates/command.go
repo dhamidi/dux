d608fdd482f67c7b768ab107e334bff9cf74d23f
Dario Hamidi <dario@gowriteco.de>
2018-03-01 21:23:28 +0200

    Make command template more generic

--- cmd/dux/blueprints/command/templates/command.go
+++ cmd/dux/blueprints/command/templates/command.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/dhamidi/dux"
+
 // TODO: Command{{.name.Title }}{{$typeName := (printf "Command%s" .name.Title)}}
 type {{$typeName}} struct {}
 
@@ -11,19 +13,5 @@ func (c *{{$typeName}}) CommandDescription() string { return `{{.description}}` 
 
 // Execute {{.description}}
 func (c *{{$typeName}}) Execute(ctx *dux.Context, args []string) error {
-	blueprintName := args[0]
-	blueprint, err := ctx.LoadBlueprint(blueprintName)
-	if err != nil {
-		return err
-	}
-	if err := blueprint.ParseArgs(args[1:]); err != nil {
-		return err
-	}
-
-	result := blueprint.Render(ctx)
-	if result.HasError() {
-		return result
-	}
-
-	return blueprint.CopyFilesToDestination(ctx, result)
+        return fmt.Errorf("Command not implemented")
 }

c082990b5d7d29596b778617833a25e5deba82cf
Dario Hamidi <dario@gowriteco.de>
2018-03-14 18:42:38 +0200

    Use event subscriptions to show what dux does

--- cmd/dux/main.go
+++ cmd/dux/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/dhamidi/dux"
@@ -12,6 +13,14 @@ func main() {
 	app.Execute(&dux.CreateBlueprint{Name: "example"})
 	app.Execute(&dux.DefineBlueprintTemplate{BlueprintName: "example", TemplateName: "example.tmpl", Contents: "hello, world"})
 	app.Execute(&dux.DefineBlueprintFile{BlueprintName: "example", FileName: "EXAMPLE", TemplateName: "example.tmpl"})
+
 	cliApp := cli.NewCLI(app)
+	app.EventStore.Subscribe(func(e *dux.Event) {
+		if e.Error == nil {
+			fmt.Printf("%s %v\n", e.Name, e.Payload)
+			return
+		}
+		cliApp.ShowError(e.Error)
+	})
 	cliApp.Execute(cli.NewCommandNew(), os.Args[1:])
 }
